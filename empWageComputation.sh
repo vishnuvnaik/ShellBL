224c34b78cd389be290bb9d5369c199a1270f90b
vishnuvnaik <vishnuvdev17@gmail.com>
2020-03-13 17:53:17 +0530

    [Vishnu]Add.Added Wage calculation by using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,19 +9,19 @@ empcheck=$(( RANDOM%2 )) #random number generation
 if [ $empcheck -eq $IsPRESENT ] #condition check for employee attendance
 then
     hourCheck=$(( RANDOM%3 ))
-    if [ $hourCheck -eq $isFullTime ] #check for fulltime employee
-    then
-        workingHours=8 #fulltime working hours
-	totalWages=$(( workingHours * wages ))
-	echo "full time wage = $totalWages" #printing wages
-    elif [ $hourCheck -eq $isPartTime ]  #parttime check
-	then
-  	    workingHours=4
-	    totalWages=$(( workingHours * wages ))
-	    echo "parttime wage = $totalWages" #printing part time wages
-    else
+    case $hourCheck in
+         $isFullTime ) #check for fulltime employee
+           workingHours=8 #fulltime working hours
+	   totalWages=$(( workingHours * wages ))
+	   echo "full time wage = $totalWages" #printing wages
+	;;
+        $isPartTime )  #parttime check
+  	   workingHours=4
+	   totalWages=$(( workingHours * wages ))
+	   echo "parttime wage = $totalWages" #printing part time wages
+	;;
+    esac
 	echo "employee is Absent"
-    fi
 else
-    echo "employee is absent"
+   echo "employee is absent"
 fi
