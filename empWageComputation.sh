11acb812f20e0ccd33df3f4b9736d0007442b12e
vishnuvnaik <vishnuvdev17@gmail.com>
2020-03-13 15:49:02 +0530

    [Vishnu]Add.Added daily employee wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash
 echo " Welcome to employee wage computation "
-IsPRESENT = 1 #to check with the random number generated
-empcheck = $(( RANDOM%2 )) #random number generation
+read -p "enter the hours present  " hours
+fulldayhour=8
+wages=20
+IsPRESENT=1 #to check with the random number generated
+empcheck=$(( RANDOM%2 )) #random number generation
 if [ $empcheck -eq $IsPRESENT ] #condition check
-then 
+then
 	echo "Employee present"
+        if [ $hours -eq 8 ] #condition for fulldayhour
+        then
+	     totalWages=$(( fulldayhour*wages ))
+	     echo "$totalWages"
+        else
+	     totalWages=$(( wages*hours )) #condition for other than fulldayhour
+             echo "$totalWages"
+        fi
 else
 	echo "Employee absent"
 fi
