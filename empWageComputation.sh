e10c1205f3e4ae036bde885bf1100726737b30b4
vishnuvnaik <vishnuvdev17@gmail.com>
2020-03-16 10:39:06 +0530

    [Vishnu]Add.Added store day and daily wage along with total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,6 +9,7 @@ totalwages=0
 workingHours=0
 isFullTime=1
 isPartTime=2
+declare -A dailyWagee
 getemphrs() { #function
     empCheck=$(( RANDOM%3 ))
     case $empCheck in
@@ -20,16 +21,18 @@ getemphrs() { #function
 	   ;;
     esac
         echo "$workingHoursperday"
-}
+} 
 while (( $workingdays<$totalworkingDays&&$workingHours<$totalWorkingHours )) #loop to check the condition of days and hours
 do
-       	empCheck=$(( RANDOM%3 ))
+	       	empCheck=$(( RANDOM%3 ))
+
         workingHoursperday="$( getemphrs $empCheck )"
 	workingHours=$(( $workingHoursperday+$workingHours )); #compuation and printing
 	(( workingdays++))
-        dailyWage[totalwagesperday]=$(( $workingHoursperday*$wagesperhour )) #Array for daily wage
+        day="Day"
 done
+dailyWagee["$day"]=$(( $workingHoursperday*$wagesperhour )) #Array for daily wage
 totalwages=$(( $workingHours*$wagesperhour )) #calculation of  total wage
-dailyWage[totalwagespermonth]=$totalwages #totalwage is stored in the array
-echo "${dailyWage[@]}" #printing array
-
+dailyWagee[permonth]=$(( $totalwages )) #totalwage is stored in the array
+echo "${dailyWagee[@]}" #printing array
+echo "${!dailyWagee[@]}"
