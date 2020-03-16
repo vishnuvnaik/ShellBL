63e0ecd1056bc7decdc53e352122427058715577
vishnuvnaik <vishnuvdev17@gmail.com>
2020-03-16 12:35:47 +0530

    [Vishnu]Refactor added function for calculating wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 echo " Welcome to employee wage computation "
-totalWorkingHours=100
+totalWorkingHours=100 #declaring constants
 wagesperhour=20
 totalworkingDays=20
-totalwagesperday=0
+isFullTime=1
+isPartTime=2
+totalwagesperday=0 #declaring constants
 workingdays=0
 totalwages=0
 workingHours=0
-isFullTime=1
-isPartTime=2
 declare -A dailyWagee #dictionary declared
 getemphrs() { #function
     empCheck=$(( RANDOM%3 ))
@@ -21,18 +21,21 @@ getemphrs() { #function
 	   ;;
     esac
         echo "$workingHoursperday"
-} 
+}
+getwages() {
+totalwages=$(( $workingHours*$wagesperhour )) #calculation of  total wage
+dailyWagee["$day $workingdays"]=$(( $workingHoursperday*$wagesperhour )) #Dictionary for daily wage
+
+}
 while (( $workingdays<$totalworkingDays&&$workingHours<$totalWorkingHours )) #loop to check the condition of days and hours
 do
-	       	empCheck=$(( RANDOM%3 ))
-
+	empCheck=$(( RANDOM%3 ))
         workingHoursperday="$( getemphrs $empCheck )"
 	workingHours=$(( $workingHoursperday+$workingHours )); #compuation and printing
 	(( workingdays++))
         day="Day"
 done
-dailyWagee["$day $workingdays"]=$(( $workingHoursperday*$wagesperhour )) #Dictionary for daily wage
-totalwages=$(( $workingHours*$wagesperhour )) #calculation of  total wage
+getwages
 dailyWagee[permonth]=$(( $totalwages )) #totalwage is stored in the dictionary
 echo "${dailyWagee[@]}" #printing dictionary
 echo "${!dailyWagee[@]}"
